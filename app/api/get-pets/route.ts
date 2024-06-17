6c55ab57b34cc1d8531d27fcf1e301f6dcdafa68
clairesbaffe <claire.sbaffe@efrei.net>
2024-06-17 11:34:59 +0200

    get pets 1.0.1

--- app/api/get-pets/route.ts
+++ app/api/get-pets/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   try {
-    const result = await sql`SELECT * FROM pets;`;
-    return NextResponse.json({ result }, { status: 200 });
+    const { rows, fields } = await sql`SELECT * FROM pets`;
+    return NextResponse.json({ rows }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
