fbb736a6e27e53293a2bf9f000bee8a650dfae23
clairesbaffe <claire.sbaffe@efrei.net>
2024-06-17 11:58:30 +0200

    get data from front

--- app/ruches/[id]/page.tsx
+++ app/ruches/[id]/page.tsx
@@ -1,43 +1,36 @@
-'use client'
+'use client';
 
 import { demos } from '#/lib/demos';
 import { routes } from '#/lib/routes';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 
+export default async function Page() {
+  const pathname = usePathname();
 
-export default function Page() {
+  const fetchData: any = async () => {
+    const response = await fetch(
+      'https://next-js-starter-lyart.vercel.app/api/get-pets',
+    );
+    const data = await response.json();
 
+    console.log(data);
+  };
 
-    const pathname = usePathname()
+  fetchData();
 
   return (
     <div className="space-y-8">
-
       <div className="space-y-10 text-white">
+        <h1>Ruche {pathname}</h1>
 
-        <h1>Ruche {pathname} .</h1>
-
-        {
-        /* {routes[1].items.map((item) => {
-          return (
-            <Link
-              href={`/${item.slug}`}
-              key={`Ruche ${item.slug}`}
-              className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
-            >
-              <div className="font-medium text-gray-200 group-hover:text-gray-50">
-                {`Ruche ${item.slug}`}
-              </div>
-
-              {item.description ? (
-                <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
-                  {item.description}
-                </div>
-              ) : null}
-            </Link>
-          );
-        })} */}
+        {/* <div>
+          {
+            data.rows.map((section) => {
+              return ()
+            }
+        }
+        </div> */}
       </div>
     </div>
   );
