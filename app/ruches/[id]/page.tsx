3dc14b3f19f2d6a96ba9c2ba3e70f02c4e336051
clairesbaffe <claire.sbaffe@efrei.net>
2024-06-17 16:43:09 +0200

    get all data 1.0

--- app/ruches/[id]/page.tsx
+++ app/ruches/[id]/page.tsx
@@ -5,33 +5,10 @@ import { usePathname } from 'next/navigation';
 export default async function Page() {
   const pathname = usePathname();
 
-  const fetchData: any = async () => {
-    const response = await fetch(
-      'https://next-js-starter-lyart.vercel.app/api/get-pets',
-    );
-    const data = await response.json();
-
-    return data;
-  };
-
-  const data = await fetchData();
-
   return (
     <div className="space-y-8">
       <div className="space-y-10 text-white">
         <h1>Ruche {pathname}</h1>
-
-        <div>
-          {data.rows.map((row: any) => {
-            return (
-              <div>
-                <p>Name : {row.name}</p>
-                <p>Owner : {row.owner}</p>
-                <br />
-              </div>
-            );
-          })}
-        </div>
       </div>
     </div>
   );
