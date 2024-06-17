a91528c495f76624d4bc4338b94960f0b6ce034a
clairesbaffe <claire.sbaffe@efrei.net>
2024-06-17 16:51:07 +0200

    get slug from url

--- app/ruches/[id]/page.tsx
+++ app/ruches/[id]/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 export default async function Page() {
+  const router = useRouter();
   const pathname = usePathname();
 
   return (
     <div className="space-y-8">
       <div className="space-y-10 text-white">
-        <h1>Ruche {pathname}</h1>
+        <h1>Ruche {router.query.slug}</h1>
       </div>
     </div>
   );
