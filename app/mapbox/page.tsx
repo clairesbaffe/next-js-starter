23dfd6721bfce13e1ab59a76639256c587ac544c
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-06-18 13:32:48 +0200

    HOTFIX mapbox 1.0.0

--- app/mapbox/page.tsx
+++ app/mapbox/page.tsx
@@ -1,38 +1,24 @@
 'use client';
 
-import Head from 'next/head';
-// import styles from '../styles/Page.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 export default function Page() {
-  const [pageIsMounted, setPageIsMounted] = useState(false);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
 
   useEffect(() => {
+    if (map.current) return; // Si la carte est déjà initialisée, ne pas ré-initialiser
+
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY;
-    setPageIsMounted(true);
-    const map = new mapboxgl.Map({
-      container: 'my-map',
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
     });
   }, []);
 
   return (
-    <div>
-      {/* className={styles.container} */}
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <main>
-        {/* className={styles.main} */}
-        <div id="my-map" style={{ height: 500, width: 500 }} />
-      </main>
-    </div>
+    <div ref={mapContainer} id="my-map" style={{ height: 500, width: 800 }} />
   );
 }
