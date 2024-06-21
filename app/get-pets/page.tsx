cbb6450c7c1aa38e88af1ef5b5d21ba412175b8b
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-06-21 12:13:19 +0200

    FIX comment pets table related code

--- app/get-pets/page.tsx
+++ app/get-pets/page.tsx
@@ -1,19 +1,19 @@
 export default async function Page() {
-  const fetchData: any = async () => {
-    const response = await fetch(
-      'https://next-js-starter-lyart.vercel.app/api/get-pets',
-    );
-    const data = await response.json();
+  // const fetchData: any = async () => {
+  //   const response = await fetch(
+  //     'https://next-js-starter-lyart.vercel.app/api/get-pets',
+  //   );
+  //   const data = await response.json();
 
-    return data;
-  };
+  //   return data;
+  // };
 
-  const data = await fetchData();
+  // const data = await fetchData();
 
   return (
     <div className="space-y-8">
       <div className="space-y-10 text-white">
-        <div>
+        {/* <div>
           {data.rows.map((row: any) => {
             return (
               <div>
@@ -23,7 +23,7 @@ export default async function Page() {
               </div>
             );
           })}
-        </div>
+        </div> */}
       </div>
     </div>
   );
