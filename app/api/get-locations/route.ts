183f533581a128feeb584f52baac94333368a724
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-06-21 10:23:33 +0200

    FEAT redeploy every 10mn + REVERT deactivate cache on API requests

--- app/api/get-locations/route.ts
+++ app/api/get-locations/route.ts
@@ -4,29 +4,8 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const { rows, fields } = await sql`SELECT * FROM locations`;
-
-    const response = NextResponse.json({ rows }, { status: 200 });
-
-    // Définir les en-têtes pour désactiver le cache
-    response.headers.set(
-      'Cache-Control',
-      'no-store, no-cache, must-revalidate, proxy-revalidate',
-    );
-    response.headers.set('Pragma', 'no-cache');
-    response.headers.set('Expires', '0');
-
-    return response;
+    return NextResponse.json({ rows }, { status: 200 });
   } catch (error) {
-    const response = NextResponse.json({ error }, { status: 500 });
-
-    // Définir les en-têtes pour désactiver le cache en cas d'erreur également
-    response.headers.set(
-      'Cache-Control',
-      'no-store, no-cache, must-revalidate, proxy-revalidate',
-    );
-    response.headers.set('Pragma', 'no-cache');
-    response.headers.set('Expires', '0');
-
-    return response;
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
