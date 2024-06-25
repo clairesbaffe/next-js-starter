e4380f66c7d3fcb3de6b7be1cfc13431f41e4618
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-06-25 16:12:15 +0200

    FEAT add get-locations-by-tracker-id api route

---
+++ app/api/(historique_Tracker)/get-locations-by-tracker-id/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const dynamic = 'force-dynamic';
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const tracker_id = searchParams.get('tracker_id');
+
+  try {
+    if (!tracker_id) throw new Error('Id du tracker requis');
+    else {
+      const locations = await prisma.historique_Tracker.findMany({
+        where: {
+          tracker_id: parseInt(tracker_id),
+        },
+      });
+
+      return NextResponse.json({ locations }, { status: 200 });
+    }
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
