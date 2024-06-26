3b0673e0f2d536572dadb2ecd2e01d7e137955a4
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-06-26 16:44:19 +0200

    FEAT change tracker mode

---
+++ app/components/TrackerItem.tsx
@@ -0,0 +1,102 @@
+// components/TrackerItem.tsx
+'use client'; // Indique que ce composant est un composant client
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEllipsis,
+  faCrosshairs,
+  faPause,
+  faPowerOff,
+  faPlay,
+} from '@fortawesome/free-solid-svg-icons';
+
+function renderModeDescription(mode: string): string {
+  switch (mode) {
+    case 'INACTIF':
+      return 'Tracker éteint';
+    case 'FONCTIONNEL':
+      return 'Tracker en marche';
+    case 'PAUSE':
+      return 'Tracker en pause';
+    case 'TRACKING':
+      return 'Tracking';
+    default:
+      return 'Unknown mode';
+  }
+}
+
+async function handleModeChange(mode: string, tracker_id: number) {
+  console.log('Changing mode...');
+  let new_mode;
+  switch (mode) {
+    case 'INACTIF':
+      new_mode = 'FONCTIONNEL';
+      break;
+    case 'FONCTIONNEL':
+      new_mode = 'PAUSE';
+      break;
+    case 'PAUSE':
+      new_mode = 'FONCTIONNEL';
+      break;
+    case 'TRACKING':
+      new_mode = 'FONCTIONNEL';
+      break;
+  }
+  await fetch(
+    `http://localhost:3000/api/update-tracker?id=${tracker_id}&mode=${new_mode}`,
+  );
+}
+
+export default function TrackerItem({ tracker }: { tracker: any }) {
+  return (
+    <div className="tracker-container">
+      <div className="left-side">
+        <h1>{tracker.nom}</h1>
+        <p>{renderModeDescription(tracker.mode)}</p>
+        <p>Rucher : {tracker.rucher.rucher.nom}</p>
+        <p>ID : {tracker.id}</p>
+      </div>
+      <div className="right-side">
+        <FontAwesomeIcon className="tracker-menu-icon" icon={faEllipsis} />
+
+        {tracker.mode === 'INACTIF' && (
+          <div onClick={() => handleModeChange(tracker.mode, tracker.id)}>
+            <FontAwesomeIcon
+              icon={faPowerOff}
+              className="tracker-mode-icon"
+              id="off-mode"
+            />
+          </div>
+        )}
+
+        {tracker.mode === 'FONCTIONNEL' && (
+          <div onClick={() => handleModeChange(tracker.mode, tracker.id)}>
+            <FontAwesomeIcon
+              icon={faPlay}
+              className="tracker-mode-icon"
+              id="on-mode"
+            />
+          </div>
+        )}
+        {tracker.mode === 'PAUSE' && (
+          <div onClick={() => handleModeChange(tracker.mode, tracker.id)}>
+            <FontAwesomeIcon
+              icon={faPause}
+              className="tracker-mode-icon"
+              id="pause-mode"
+            />
+          </div>
+        )}
+        {tracker.mode === 'TRACKING' && (
+          <div onClick={() => handleModeChange(tracker.mode, tracker.id)}>
+            <FontAwesomeIcon
+              icon={faCrosshairs}
+              className="tracker-mode-icon"
+              id="tracking-mode"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- app/liste-trackers/page.tsx
+++ app/liste-trackers/page.tsx
@@ -1,13 +1,6 @@
-import Link from 'next/link';
+// pages/Page.tsx
 import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faEllipsis,
-  faCrosshairs,
-  faPause,
-  faPowerOff,
-  faPlay,
-} from '@fortawesome/free-solid-svg-icons';
+import TrackerItem from '../components/TrackerItem'; // Importez le composant client
 
 async function fetchTrackers() {
   const response = await fetch(
@@ -16,27 +9,13 @@ async function fetchTrackers() {
   const data = await response.json();
   return data;
 }
+
 async function fetchRucher(rucher_id: Number) {
   const response = await fetch(
     `http://localhost:3000/api/get-rucher-by-id?rucher_id=${rucher_id}`,
   );
   const data = await response.json();
   return data;
-}
-
-function renderModeDescription(mode: string): string {
-  switch (mode) {
-    case 'INACTIF':
-      return 'Tracker éteint';
-    case 'FONCTIONNEL':
-      return 'Tracker en marche';
-    case 'PAUSE':
-      return 'Tracker en pause';
-    case 'TRACKING':
-      return 'Tracking';
-    default:
-      return 'Unknown mode';
-  }
 }
 
 export default async function Page() {
@@ -47,67 +26,16 @@ export default async function Page() {
       return { ...tracker, rucher };
     }),
   );
-  console.log(JSON.stringify(trackersWithRucher, null, 2));
+  // console.log(JSON.stringify(trackersWithRucher, null, 2));
 
   return (
     <div className="space-y-8">
       <h1 className="text-xl font-medium text-gray-300">Trackers</h1>
 
       <div className="trackers-list">
-        {trackersWithRucher.map((tracker: any) => {
-          return (
-            <div className="tracker-container">
-              <div className="left-side">
-                <h1>{tracker.nom}</h1>
-                <p>{renderModeDescription(tracker.mode)}</p>
-                <p>Rucher : {tracker.rucher.rucher.nom}</p>
-                <p>ID : {tracker.id}</p>
-              </div>
-              <div className="right-side">
-                <FontAwesomeIcon
-                  className="tracker-menu-icon"
-                  icon={faEllipsis}
-                />
-
-                {tracker.mode === 'INACTIF' && (
-                  // <Link
-                  //   href={`/"http://localhost:3000/api/update-tracker?id=1&mode=FONCTIONNEL"`}
-                  // >
-                  //   <div>
-                  <FontAwesomeIcon
-                    icon={faPowerOff}
-                    className="tracker-mode-icon"
-                    id="off-mode"
-                  />
-                  //   </div>
-                  // </Link>
-                )}
-
-                {tracker.mode === 'FONCTIONNEL' && (
-                  <FontAwesomeIcon
-                    icon={faPlay}
-                    className="tracker-mode-icon"
-                    id="on-mode"
-                  />
-                )}
-                {tracker.mode === 'PAUSE' && (
-                  <FontAwesomeIcon
-                    icon={faPause}
-                    className="tracker-mode-icon"
-                    id="pause-mode"
-                  />
-                )}
-                {tracker.mode === 'TRACKING' && (
-                  <FontAwesomeIcon
-                    icon={faCrosshairs}
-                    className="tracker-mode-icon"
-                    id="tracking-mode"
-                  />
-                )}
-              </div>
-            </div>
-          );
-        })}
+        {trackersWithRucher.map((tracker: any) => (
+          <TrackerItem key={tracker.id} tracker={tracker} />
+        ))}
       </div>
     </div>
   );
