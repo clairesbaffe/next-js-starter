6959cf0553646746f8e284db50d386e8f065bead
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-07-03 16:35:47 +0200

    HOTFIX trackers list style bug when less than 3 displayed

--- app/components/TrackersList.tsx
+++ app/components/TrackersList.tsx
@@ -5,12 +5,10 @@ import { TrackersListProps } from './types';
 
 function TrackersList({ initialTrackers }: TrackersListProps) {
   return (
-    <div>
-      <div className="trackers-list">
-        {initialTrackers.map((tracker: any) => (
-          <TrackerItem key={tracker.id} tracker={tracker} />
-        ))}
-      </div>
+    <div className="trackers-list">
+      {initialTrackers.map((tracker: any) => (
+        <TrackerItem key={tracker.id} tracker={tracker} />
+      ))}
     </div>
   );
 }
