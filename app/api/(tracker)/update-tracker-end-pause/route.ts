dc848ad415578b5a8327b873df612e4adf300859
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-07-04 11:35:35 +0200

    FEAT API routes for pause duration

---
+++ app/api/(tracker)/update-tracker-end-pause/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse } from 'next/server';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  const now = new Date();
+
+  try {
+    // Trouver tous les trackers en mode PAUSE dont la pause est terminée
+    const pausedTrackers = await prisma.tracker.findMany({
+      where: {
+        mode: 'PAUSE',
+        pause_end_time: {
+          lte: now,
+        },
+      },
+    });
+
+    if (pausedTrackers.length > 0) {
+      let getTrackerIds = (pausedTrackers: any) => {
+        let ids = pausedTrackers.map((tracker: any) => tracker.id);
+        return ids;
+      };
+      const trackerIds: any = getTrackerIds(pausedTrackers);
+
+      // Mettre à jour ces trackers en mode FONCTIONNEL
+      for (const tracker of pausedTrackers) {
+        await prisma.tracker.update({
+          where: { id: tracker.id },
+          data: {
+            mode: 'FONCTIONNEL',
+            pause_duration: null,
+            pause_end_time: null,
+          },
+        });
+      }
+
+      return NextResponse.json(
+        `Les trackers [${trackerIds.join()}] ont terminé leur pause`,
+        {
+          status: 200,
+        },
+      );
+    } else {
+      return NextResponse.json(`Aucun tracker ne doit terminer sa pause`, {
+        status: 200,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- app/api/(tracker)/update-tracker-mode/route.ts
+++ app/api/(tracker)/update-tracker-mode/route.ts
@@ -10,37 +10,72 @@ function isValidMode(mode: string): mode is Mode {
   return validModes.includes(mode as Mode);
 }
 
+function addSeconds(date: Date, seconds: number) {
+  date.setSeconds(date.getSeconds() + seconds);
+  return date;
+}
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
   const mode = searchParams.get('mode');
+  const pause_duration = searchParams.get('pause_duration');
 
   try {
     if (!id) throw new Error('Id du tracker requis');
     else {
       if (!mode) {
-        throw new Error('Mode requis requis');
+        throw new Error('Mode requis');
       } else {
         if (mode && !isValidMode(mode)) {
           throw new Error("Le mode n'est pas valide");
         } else if (mode && isValidMode(mode)) {
           // MODIFIER LE MODE
-          await prisma.tracker.update({
-            where: {
-              id: parseInt(id),
-            },
-            data: {
-              mode: mode,
-            },
-          });
-          return NextResponse.json(`Le tracker ${id} est en mode ${mode}`, {
-            status: 200,
-          });
+          if (mode === 'PAUSE') {
+            if (!pause_duration) {
+              throw new Error('Durée de la pause requise');
+            } else {
+              let date = new Date();
+
+              const pause_end_time = addSeconds(date, parseInt(pause_duration));
+
+              await prisma.tracker.update({
+                where: {
+                  id: parseInt(id),
+                },
+                data: {
+                  mode: mode,
+                  pause_duration: parseInt(pause_duration),
+                  pause_end_time: pause_end_time,
+                },
+              });
+              return NextResponse.json(
+                `Le tracker ${id} est en mode ${mode} pendant ${pause_duration} secondes`,
+                {
+                  status: 200,
+                },
+              );
+            }
+          } else {
+            await prisma.tracker.update({
+              where: {
+                id: parseInt(id),
+              },
+              data: {
+                mode: mode,
+              },
+            });
+            return NextResponse.json(`Le tracker ${id} est en mode ${mode}`, {
+              status: 200,
+            });
+          }
         }
       }
     }
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    const errorMessage =
+      error instanceof Error ? error.message : 'Internal Serveur Error';
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
