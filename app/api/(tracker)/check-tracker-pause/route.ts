250f03576bdcca54764f8d603ac19745ae944200
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-07-08 14:21:47 +0200

    HOTFIX force-dynamic on check-tracker-pause route

--- app/api/(tracker)/check-tracker-pause/route.ts
+++ app/api/(tracker)/check-tracker-pause/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('trackerId');
