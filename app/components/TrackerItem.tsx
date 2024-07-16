85adc578a6157a2e7dbdb8cc03e597e535dd103f
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-07-16 09:39:53 +0200

    FEAT change colour when pause tracking + popup onclick mode tracking

--- app/components/TrackerItem.tsx
+++ app/components/TrackerItem.tsx
@@ -16,7 +16,10 @@ import 'reactjs-popup/dist/index.css';
 import Modal from 'react-modal';
 import styles from './Modal.module.css';
 
-function renderModeDescription(mode: string): string {
+function renderModeDescription(
+  mode: string,
+  pause_tracking: boolean | undefined,
+): string {
   switch (mode) {
     case 'INACTIF':
       return 'Tracker éteint';
@@ -25,37 +28,12 @@ function renderModeDescription(mode: string): string {
     case 'PAUSE':
       return 'Tracker en pause';
     case 'TRACKING':
+      if (pause_tracking) return 'Tracking en pause';
       return 'Tracking';
     default:
       return 'Unknown mode';
   }
 }
-
-const sendMessage = async (ind: number, time: number, topic: string) => {
-  const message = { ind: ind, time: time };
-
-  try {
-    const response = await fetch('/api/publish', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ topic: topic, message: message }),
-    });
-
-    const data = await response.json();
-
-    if (!data.success) {
-      throw new Error(
-        data.error || 'An error occurred while publishing the message',
-      );
-    }
-
-    console.log(data);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
 
 async function handleModeChange(
   tracker: any,
@@ -135,17 +113,74 @@ async function handleModeChange(
   return new_mode;
 }
 
+async function handleStartPauseTracking(
+  tracker: any,
+  selectedOption: string,
+  event?: any,
+) {
+  let pause_duration;
+  let hours;
+  let minutes;
+  let seconds;
+  let pause;
+
+  if (event) {
+    event.preventDefault();
+    const form = event.target;
+    if (selectedOption === 'pause') {
+      hours = form.pause_duration_h.value;
+      minutes = form.pause_duration_m.value;
+      seconds = form.pause_duration_s.value;
+      pause_duration =
+        parseInt(seconds) + parseInt(minutes) * 60 + parseInt(hours) * 3600;
+    }
+  }
+
+  if (selectedOption === 'pause') {
+    const submitData = {
+      trackerId: tracker.id,
+      duration: pause_duration,
+    };
+    await fetch('https://next-js-starter-lyart.vercel.app/api/pause-tracking', {
+      method: 'PATCH',
+      body: JSON.stringify(submitData),
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  } else {
+    const submitData = {
+      trackerId: tracker.id,
+    };
+    await fetch(
+      'https://next-js-starter-lyart.vercel.app/api/cancel-tracking',
+      {
+        method: 'PATCH',
+        body: JSON.stringify(submitData),
+        headers: {
+          'content-type': 'application/json',
+        },
+      },
+    );
+  }
+
+  window.location.reload();
+}
+
 async function handleTrackerDelete(tracker_id: number) {
   const submitData = {
     id: tracker_id,
   };
-  await fetch('http://localhost:3000/api/delete-tracker-by-id', {
-    method: 'DELETE',
-    body: JSON.stringify(submitData),
-    headers: {
-      'content-type': 'application/json',
+  await fetch(
+    'https://next-js-starter-lyart.vercel.app/api/delete-tracker-by-id',
+    {
+      method: 'DELETE',
+      body: JSON.stringify(submitData),
+      headers: {
+        'content-type': 'application/json',
+      },
     },
-  });
+  );
 
   window.location.reload();
 }
@@ -156,6 +191,13 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
     useState(false);
   const [modalToggleTrackerPauseIsOpen, setModalToggleTrackerPauseIsOpen] =
     useState(false);
+  const [modalToggleTrackingPauseIsOpen, setModalToggleTrackingPauseIsOpen] =
+    useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+  const [pauseDuration, setPauseDuration] = useState('');
+  const [pauseHours, setPauseHours] = useState(0);
+  const [pauseMinutes, setPauseMinutes] = useState(0);
+  const [pauseSeconds, setPauseSeconds] = useState(44);
 
   const openModalDeleteTracker = () => {
     setModalDeleteTrackerIsOpen(true);
@@ -169,6 +211,20 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
   };
   const closeModalToggleTrackerPause = () => {
     setModalToggleTrackerPauseIsOpen(false);
+  };
+
+  const openModalToggleTrackingPause = () => {
+    setModalToggleTrackingPauseIsOpen(true);
+  };
+  const closeModalToggleTrackingPause = () => {
+    setModalToggleTrackingPauseIsOpen(false);
+  };
+
+  const handleOptionChange = (e: any) => {
+    setSelectedOption(e.target.value);
+  };
+  const handlePauseDurationChange = (setter: any) => (e: any) => {
+    setter(e.target.value);
   };
 
   return (
@@ -213,7 +269,8 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
                   required
                   min={0}
                   max={59}
-                  defaultValue={0}
+                  value={pauseHours}
+                  onChange={handlePauseDurationChange(setPauseHours)}
                 />
               </div>
 
@@ -228,7 +285,8 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
                   required
                   min={0}
                   max={59}
-                  defaultValue={0}
+                  value={pauseMinutes}
+                  onChange={handlePauseDurationChange(setPauseMinutes)}
                 />
               </div>
 
@@ -244,7 +302,8 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
                   required
                   min={0}
                   max={59}
-                  defaultValue={10}
+                  value={pauseSeconds}
+                  onChange={handlePauseDurationChange(setPauseSeconds)}
                 />
               </div>
             </div>
@@ -258,10 +317,114 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
         </div>
       </Modal>
 
+      <Modal
+        isOpen={modalToggleTrackingPauseIsOpen}
+        onRequestClose={closeModalToggleTrackingPause}
+        ariaHideApp={false}
+        className={styles.modalContainer}
+        overlayClassName={styles.modalOverlay}
+      >
+        <div className="componentContainer space-y-8">
+          <h1>Mettre en pause ou annuler le tracking ?</h1>
+          <FontAwesomeIcon
+            className="closeAddTrackerModal"
+            onClick={closeModalToggleTrackingPause}
+            icon={faXmark}
+          />
+          <form
+            onSubmit={(event) =>
+              handleStartPauseTracking(tracker, selectedOption, event)
+            }
+            className="formContainer space-y-6"
+          >
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="pause"
+                  checked={selectedOption === 'pause'}
+                  onChange={handleOptionChange}
+                />
+                Mettre en pause
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="cancel"
+                  checked={selectedOption === 'cancel'}
+                  onChange={handleOptionChange}
+                />
+                Annuler
+              </label>
+            </div>
+
+            {selectedOption === 'pause' && (
+              <div>
+                <div className="displayRow">
+                  <div className="displayColumn">
+                    <label htmlFor="pause_duration_h">heures</label>
+                    <input
+                      className="input-style"
+                      type="number"
+                      name="pause_duration_h"
+                      id="pause_duration_h"
+                      required
+                      min={0}
+                      max={59}
+                      value={pauseHours}
+                      onChange={handlePauseDurationChange(setPauseHours)}
+                    />
+                  </div>
+
+                  <span> : </span>
+                  <div className="displayColumn">
+                    <label htmlFor="pause_duration_m">minutes</label>
+                    <input
+                      className="input-style"
+                      type="number"
+                      name="pause_duration_m"
+                      id="pause_duration_m"
+                      required
+                      min={0}
+                      max={59}
+                      value={pauseMinutes}
+                      onChange={handlePauseDurationChange(setPauseMinutes)}
+                    />
+                  </div>
+
+                  <span> : </span>
+
+                  <div className="displayColumn">
+                    <label htmlFor="pause_duration_s">secondes</label>
+                    <input
+                      className="input-style"
+                      type="number"
+                      name="pause_duration_s"
+                      id="pause_duration_s"
+                      required
+                      min={0}
+                      max={59}
+                      value={pauseSeconds}
+                      onChange={handlePauseDurationChange(setPauseSeconds)}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+            <input
+              type="submit"
+              value="Enregistrer"
+              className="rounded border border-gray-400 bg-white px-4 py-2 font-semibold text-gray-800 shadow hover:bg-gray-100"
+            />
+          </form>
+        </div>
+      </Modal>
+
       <div className="left-side">
         <h1>{tracker.nom}</h1>
-        <p>{renderModeDescription(trackerMode)}</p>
-        {tracker.mode === 'PAUSE' && (
+        <p>{renderModeDescription(trackerMode, tracker.pause_tracking)}</p>
+        {(tracker.mode === 'PAUSE' ||
+          (tracker.mode === 'TRACKING' && tracker.pause_tracking)) && (
           <p>
             Fin de la pause :{' '}
             {new Date(tracker.pause_end_time).toLocaleTimeString('fr-FR', {
@@ -395,16 +558,25 @@ export default function TrackerItem({ tracker }: { tracker: any }) {
             />
           </div>
         )}
-        {trackerMode === 'TRACKING' && (
-          <div
-            onClick={() =>
-              setTrackerMode(handleModeChange(tracker, trackerMode, false))
-            }
-          >
+        {trackerMode === 'TRACKING' && !tracker.pause_tracking && (
+          <div onClick={openModalToggleTrackingPause}>
             <FontAwesomeIcon
               icon={faCrosshairs}
               className="tracker-mode-icon"
               id="tracking-mode"
+            />
+          </div>
+        )}
+        {trackerMode === 'TRACKING' && tracker.pause_tracking && (
+          <div
+          // onClick={() =>
+          //   setTrackerMode(handleModeChange(tracker, trackerMode, false))
+          // }
+          >
+            <FontAwesomeIcon
+              icon={faCrosshairs}
+              className="tracker-mode-icon"
+              id="pause-tracking-mode"
             />
           </div>
         )}
