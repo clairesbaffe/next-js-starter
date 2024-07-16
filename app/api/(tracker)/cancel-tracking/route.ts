f2f6f772481ebc86d7a5c9344c9b7e1576c7ce02
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-07-16 09:38:47 +0200

    FEAT API routes for pause and cancel tracking

--- app/api/(tracker)/cancel-tracking/route.ts
+++ app/api/(tracker)/cancel-tracking/route.ts
@@ -1,36 +1,27 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-import { startTrackerTimer } from '../../../../lib/timer';
 import { publishMessage } from '../../../../mqttClient';
 
 const prisma = new PrismaClient();
 
-function addSeconds(date: Date, seconds: number) {
-  date.setSeconds(date.getSeconds() + seconds);
-  return date;
-}
-
 export async function PATCH(req: Request, res: any) {
   try {
-    const { trackerId, duration } = await req.json();
-
-    let date = new Date();
-    const pause_end_time = addSeconds(date, parseInt(duration));
+    const { trackerId } = await req.json();
 
     const updatedTracker = await prisma.tracker.update({
       where: { id: trackerId, mode: 'TRACKING' },
       data: {
-        pause_duration: duration,
-        pause_end_time: pause_end_time,
-        pause_tracking: true,
+        mode: 'FONCTIONNEL',
+        pause_duration: null,
+        pause_end_time: null,
+        deplacement: null,
+        pause_tracking: null,
+        date_modif_mode: new Date(),
       },
     });
 
-    // Start a timer for this tracker
-    startTrackerTimer(trackerId, duration, 'TRACKING');
-
     const topic = `t/t/${updatedTracker.id}`;
-    const messageString = `{"ind": 1, "time": ${duration}}`;
+    const messageString = `{"ind": 2}`;
     publishMessage(topic, messageString);
 
     return NextResponse.json(updatedTracker, { status: 200 });
